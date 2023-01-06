06008f6a8d848747d69624c45f01851bbd4e4f2f
Cecilia Penha <cecilia@penha.fr>
2023-01-06 13:26:49 +0000

    Add Plugins

---
+++ plugins/src/components/HelloWorld/HelloWorld.ts
@@ -0,0 +1,41 @@
+import {html, css, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {NintexPlugin} from '../../../lib/nintex-plugin';
+
+@customElement('ntx-helloworld')
+export class HelloWorld extends LitElement {
+  static styles = css`p { color: blue }`;
+
+  @property()
+  name = 'Somebody';
+
+  static getMetaConfig(): Promise<NintexPlugin> | NintexPlugin {
+    // plugin contract information
+    return {
+      controlName: 'Hello World',
+      fallbackDisableSubmit: false,
+      description: 'Hello World component',
+      iconUrl: 'one-line-text',
+      groupName: 'ByC - Simple',
+      version: '1.0',
+      properties: {
+        name: {
+          type: 'string',
+          title: 'Name',
+          description:
+            'Name for greetings',
+          defaultValue: 'somebody',
+        }
+      },
+      standardProperties: {
+        readOnly: true,
+        required: true,
+        description: true,
+      },
+    };
+  }
+
+  render() {
+    return html`<p>Hello, ${this.name}!</p>`;
+  }
+}
