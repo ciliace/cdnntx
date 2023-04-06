94e81de5e975d12c2bf98eb0909e2636caa7416d
Cécilia Penha <cecilia@penha.fr>
2023-04-06 05:00:26 +0100

    test

--- JS/NAP-UnderwriterApp.js
+++ JS/NAP-UnderwriterApp.js
@@ -1,1 +1,7 @@
-$('frame').contents().find('.logoBarWrapper').css({'display':'none'});
+$('#frame').contents().find('.logoBarWrapper').css({'display':'none'});
+
+$("#frame").on("load", function() {
+    let head = $("#frame").contents().find("head");
+    let css = '<style>div.logoBar {display:none;}</style>';
+    $(head).append(css);
+  });
