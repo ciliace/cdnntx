ba64200eb94e1c9ac73a1ed293ece1745e98a895
Cécilia Penha <cecilia@penha.fr>
2023-04-23 21:21:18 +0100

    DEMO

--- JS/NAP-HR.js
+++ JS/NAP-HR.js
@@ -13,11 +13,18 @@
 //     doc.body.innerHTML = doc.body.innerHTML + '<style>:root {--is-in-iframe:0;}</style>';
 //  }
 
-  window.onload = function() {
-    let frame = document.querySelector("div[name='HR-APP'] > div > div > iframe");
-    let doc = frame.contentDocument;
-    //doc.body.innerHTML = doc.body.innerHTML + '<style>:root {--is-in-iframe:0;}</style>';
- }
+//   window.onload = function() {
+//     let frame = document.querySelector("div[name='HR-APP'] > div > div > iframe");
+//     let doc = frame.contentDocument;
+//     //doc.body.innerHTML = doc.body.innerHTML + '<style>:root {--is-in-iframe:0;}</style>';
+//  }
+
+ var iframe = document.querySelector("div[name='HR-APP'] > div > div > iframe");
+ iframe.addEventListener('load', function() {
+   var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+   var itemToChange = iframeDocument.querySelector('body');
+   itemToChange.style.setProperty('--is-in-iframe', '0');
+ });
 
 
 // var iframe = document.querySelector("div[name='HR-APP'] > div > div > iframe");
