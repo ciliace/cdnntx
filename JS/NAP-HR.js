c33a02de59eec653f1bdccbd383bf4455a0c81a4
Cécilia Penha <cecilia@penha.fr>
2023-04-24 02:16:14 +0100

    DEMO

--- JS/NAP-HR.js
+++ JS/NAP-HR.js
@@ -6,3 +6,11 @@
    iframeRoot.setAttribute('is-in-iframe', 1);
  });
 
+ var iframe = document.querySelector("div[name='HR-APP'] > div > div > iframe");
+ iframe.addEventListener('load', function() {
+   var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+   var iframeRoot = iframe.contentDocument.documentElement;
+   iframeRoot.setAttribute('is-in-iframe', 1);
+ });
+
+
