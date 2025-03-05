ce66c86d6e74c8224c1ea11bdc3efaa138bd8f59
Cécilia Penha <cecilia@penha.fr>
2025-03-05 20:55:35 +0000

    Update K2.js

--- JS/K2.js
+++ JS/K2.js
@@ -1,3 +1,21 @@
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("https://notifications.penha.fr/notificationhub")
+    .build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    showToast(message, 'info'); // Call the showToast function
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+function sendNotification(user, message) {
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+
 function showToast(message, type) {
     toastr[type](message);
 }
