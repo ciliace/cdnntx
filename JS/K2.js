a282861dde9e5457d6384fdecd9c723da0b0b89a
Cécilia Penha <cecilia@penha.fr>
2025-03-06 01:42:12 +0000

    Update K2.js

--- JS/K2.js
+++ JS/K2.js
@@ -1,38 +1,21 @@
-$(document).ready(function () {
-    // Connect to the SignalR hub
+$(function () {
+    console.log("Connecting to SignalR...");
+
     var connection = $.hubConnection("https://notifications.penha.fr");
     var hubProxy = connection.createHubProxy("notificationhub");
 
-    // Define the function that will be called when a notification is received
     hubProxy.on("ReceiveNotification", function (message) {
-        console.log("New notification:", message);
-        //alert("New Notification: " + message);
+        console.log("New notification received:", message);
         showBrowserNotification(message);
-        
     });
 
-    // Start the connection
     connection.start()
         .done(function () {
             console.log("Connected to SignalR hub!");
-
-            // Manually define sendNotification function
-            window.sendNotification = function (message) {
-                hubProxy.invoke("SendNotification", message)
-                    .done(function () {
-                        console.log("Notification sent successfully.");
-                    })
-                    .fail(function (error) {
-                        console.error("Error sending notification:", error);
-                    });
-            };
         })
         .fail(function (error) {
             console.error("Failed to connect to SignalR:", error);
         });
-
-
-
 });
 function showBrowserNotification(message) {
     // Check if browser supports notifications
